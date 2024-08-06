36975bbb3672a44d115577a76a717d2bf1f022cb
Greg Lemrow <greg.lemrow@gmail.com>
2024-08-05 21:08:08 -0400

    tests

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_security_group" "allow-RDP" { # want to allow RDP from specified l
     from_port   = 0
     to_port     = 0
     protocol    = -1
-    cidr_blocks = ["172.31.0.0/20"]
+    cidr_blocks = ["172.31.0.0/20"] # this is a test
   }
 
   ingress {
